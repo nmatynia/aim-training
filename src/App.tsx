458653366e2190f30aeedc34d223a11f896c5a4d
nmatynia <norbert.matyniaa@gmail.com>
2022-04-18 19:23:25 +0100

    Added basic logic for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 import GameArea from './Components/GameArea';
 import Nav from './Components/Nav';
+
+interface IState{
+  score: number;
+  time: number;
+}
+
 const App: React.FC = () =>{
+
+  const [score,setScore] = useState<IState["score"]>(0)
+  const [time,setTime] = useState<IState["time"]>(0)
+  console.log("Score: ",score)
   return (
     <div className="App">
         <h1 className ="appHeader">Aim Training</h1>
         <Nav/>
-        <GameArea/>
-        
+        <GameArea setScore={setScore} score={score}/>
     </div>
   );
 }
--- src/Components/GameArea.tsx
+++ src/Components/GameArea.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "../Styles/GameArea.css";
 
-const GameArea: React.FC = () =>{
+interface IProps{
+    score:number
+    setScore:React.Dispatch<React.SetStateAction<number>>
+}
+
+const GameArea: React.FC<IProps> = ({setScore,score}) =>{
+    const randomizePosition = ():number =>{
+        const number = Math.floor(Math.random() * 59);
+        console.log(number);
+        return number;
+    }
+
+    const handleClick = () =>{
+        return ()=> setScore(prevState => prevState + 1)
+    }
     return (
         <div className="GameArea">
-            <div className="target" style={{left:"0px"}}></div>
+            <div onClick ={handleClick()}className="target" style={{left:`${randomizePosition()}vh`, top:`${randomizePosition()}vh`}}></div>
+            <div className="score">Score: {score}</div>
         </div>
     )
 }
