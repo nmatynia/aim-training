1c72160d75967e397aefa0d14312ca9438f03b0b
nmatynia <norbert.matyniaa@gmail.com>
2022-04-20 19:41:43 +0100

    Created session best element and its styling

--- src/Components/GameArea.tsx
+++ src/Components/GameArea.tsx
@@ -18,7 +18,8 @@ interface IProps{
 const GameArea: React.FC<IProps> = ({setScore,score,targetSize,mapSize,time, gameStarting,setGameStarting,gameStatus,setGameStatus}) =>{
 
     const missedClicks = useRef(0);
-    
+    const [sessionBest, setSessionBest] = useState<number>(0);
+
     const randomizePosition = ():number =>{
         const maxPosition = mapSize - targetSize + 1;
         const number = Math.floor(Math.random() * maxPosition);
@@ -66,6 +67,18 @@ const GameArea: React.FC<IProps> = ({setScore,score,targetSize,mapSize,time, gam
 
     const calculatedAccuracy = Math.round(score/(score+missedClicks.current)*100*100)/100;
 
+    
+    const sessionBestPrecent = ():JSX.Element | null =>{
+        const precent = Math.round(((score/sessionBest*100)-100)*100)/100;
+        return precent < 0 ? <span style={{color:"rgba(255, 0, 0, 0.29)"}}>({precent}%)</span> :( precent !== 0 ? <span style={{color:"#A2EEA5"}}>(+{precent}%)</span>:null)
+    } 
+
+    //if something doesnt work its because of this
+    if(!gameStatus && sessionBest<score){
+        setSessionBest(score);
+    }
+    
+
     return (
         <div className="GameArea" style={{width:`${mapSize}vh`, height:`${mapSize}vh`}}>
 
@@ -76,6 +89,7 @@ const GameArea: React.FC<IProps> = ({setScore,score,targetSize,mapSize,time, gam
             <div className="score">SCORE: {score}</div>
             {gameStarting || !gameStatus ? <div className="timer">{formattedTime}</div>:<Timer time={time} setScore={setScore} setGameStatus={setGameStatus}/>}
             {!gameStarting && !gameStatus ? <div className ="accuracy">ACCURACY: {calculatedAccuracy}%</div>: null}
+            {!gameStarting && !gameStatus ? <div className ="sessionBest">SESSION BEST: {sessionBest} {sessionBestPrecent()}</div>: null}
             
         </div>
     )
