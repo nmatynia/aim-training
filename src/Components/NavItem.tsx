2816e7588add91bc3bffe8ac843ba90890ff79b1
nmatynia <norbert.matyniaa@gmail.com>
2022-04-21 19:09:34 +0100

    Made the dropdowns close when clicking outside of them

--- src/Components/NavItem.tsx
+++ src/Components/NavItem.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useEffect, useRef, useState} from 'react';
 import { isPropertySignature } from 'typescript';
 import '../Styles/NavItem.css'
 
@@ -10,9 +10,22 @@ interface IProps{
 const NavItem:React.FC<IProps> = ({text,children,setGameStatus}) =>{
 
     const [open, setOpen] = useState<boolean>(false);
+    const dropDownRef = useRef<any>();
+
+    //closes on clicking outside
+    useEffect(()=>{
+        const closeDropdown = (e:any) =>{
+            if(!dropDownRef.current.contains(e.target)){
+                setOpen(false)
+            }
+        }
+        document.body.addEventListener('mousedown',closeDropdown);
+
+        return () => document.body.removeEventListener('mousedown',closeDropdown)
+    })
 
     return(
-        <div className={`navItem ${open?" dropButtonOpened":""}`} onClick={()=>setGameStatus(false)}>
+        <div ref={dropDownRef} className={`navItem ${open?" dropButtonOpened":""}`} onClick={()=>setGameStatus(false)}>
             <span className = {`dropButton${open?" dropButtonOpened":""}`} onClick={() => setOpen(!open)}>
                 {text} 
             </span>
